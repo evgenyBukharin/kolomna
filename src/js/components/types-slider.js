7fced1bbce954dfe3e08dd6164925173eb730110
Evgeniy Bukharin <buharinzxc123@gmail.com>
2024-06-09 15:43:44 +0500

    types section done

---
+++ src/js/components/types-slider.js
@@ -0,0 +1,28 @@
+import Swiper, { Pagination, EffectFade } from "swiper";
+Swiper.use([Pagination, EffectFade]);
+
+const sliderEl = document.querySelector(".types__slider");
+if (sliderEl) {
+	let types = ["M", "A1", "A", "B", "C", "D", "BE", "CE", "DE", "САМОХОДНЫЕ ТС"];
+	let typesSlider = new Swiper(sliderEl, {
+		spaceBetween: 30,
+		slidesPerView: 1,
+		speed: 600,
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
+		allowTouchMove: false,
+		autoHeight: true,
+		pagination: {
+			el: ".types__pagination",
+			type: "bullets",
+			clickable: true,
+			bulletClass: "types__bullet",
+			bulletActiveClass: "types__bullet-active",
+			renderBullet: function (idx, className) {
+				return `<span class="${className}">${types[idx]}</span>`;
+			},
+		},
+	});
+}
